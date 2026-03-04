3c106e77ddc7e2e1fe2480f579cdfe9f15adf611
Nayanthi Weerasuriya <nayanthiweerasuriya2003@gmail.com>
2026-03-04 19:59:58 +0530

    implmented validations for form fields

---
+++ uniconnect-backend/src/main/java/com/uniconnect/department/DepartmentService.java
@@ -0,0 +1,38 @@
+package com.uniconnect.department;
+
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+
+@Service
+public class DepartmentService {
+
+    private final DepartmentRepository departmentRepository;
+
+    public DepartmentService(DepartmentRepository departmentRepository) {
+        this.departmentRepository = departmentRepository;
+    }
+
+    public Optional<Department> getDepartmentByName(String name) {
+        return departmentRepository.findByName(name);
+    }
+
+    public Optional<Department> getDepartmentByCode(String code) {
+        return departmentRepository.findByCode(code);
+    }
+
+    public boolean validateCpmNumber(String departmentName, String cpmNumber) {
+        Optional<Department> department = departmentRepository.findByName(departmentName);
+        if (department.isPresent()) {
+            return department.get().isCpmNumberValid(cpmNumber);
+        }
+        return false;
+    }
+
+    public Department saveDepartment(Department department) {
+        return departmentRepository.save(department);
+    }
+
+    public Iterable<Department> getAllDepartments() {
+        return departmentRepository.findAll();
+    }
+}
