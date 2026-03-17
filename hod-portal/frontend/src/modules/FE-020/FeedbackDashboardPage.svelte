ba6e8258223b6d0870fc3a622e31173e3d417710
Tharuka-Gunarathne <tharukagunaratne2003@gmail.com>
2026-03-17 11:02:37 +0530

    feat(hod-portal): Feedback Review Dashboard [FE-020]

---
+++ hod-portal/frontend/src/modules/FE-020/FeedbackDashboardPage.svelte
@@ -0,0 +1,15 @@
+<script>
+  import FeedbackTable from './FeedbackTable.svelte';
+</script>
+
+<div class="page-container">
+  <FeedbackTable />
+</div>
+
+<style>
+  .page-container {
+    display: flex;
+    flex-direction: column;
+    gap: 32px;
+  }
+</style>
